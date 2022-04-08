22ba9d8ffa3955ddf4ed9e3a60a9801229a7c9d7
ankith patil <ankithpatilbusiness@gmail.com>
2022-04-08 20:30:10 +0530

    first one

--- app.py
+++ app.py
@@ -18,19 +18,29 @@ def index():
         try:
             searchString = request.form['content'].replace(" ","")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-            uClient = uReq(flipkart_url)
-            flipkartPage = uClient.read()
-            uClient.close()
-            flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
-            box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
+            uClient = uReq(flipkart_url) #requesting the web page from the flipkart website of the url
+            flipkartPage = uClient.read() #reading the web page or the same html page which u see when u right click the page into its page source
+            uClient.close() #closing the connection to the web server so that error doesnt arise
+            flipkart_html = bs(flipkartPage, "html.parser") #beautiful soup parses the web page as HTML(structured) from the original byte format data
+            # which can accessed using tags(like dictionary). u can call the html tags for which the data would be available
+            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"}) #searching for appropriate tag to redirect to the product link
+            del bigboxes[0:3] #deleting the first 3 products of the list
+            box = bigboxes[0] #accessing the the 3rd product of the list
+
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href'] # check the website for the orientation of the html ot know the how much
+            # div and a class are required to access a product. all the html classes give the area of the wesite if u rover over the inspect page of the right click of
+            #  the page. u can use the library but for knowing the basics it is being hard coded intially later u  dont need to to do this as its the same
+            #  for every search result.href contain the url of the specific product when u hover the mouse over that product
+            #  flipkart is added to the url as it doesnt contain the flipkart website name.
+
+            prodRes = requests.get(productLink) #gets the product page from the product url
             prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
+            prod_html = bs(prodRes.text, "html.parser")# parsed into structured html of the particular product page from the byte type of the inspect right click of page
             print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})# finding the html page where there is the comment section
+
+            #saving the already saved and searched ones in the file named the product name
+            #it will save in the form dictionary
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
